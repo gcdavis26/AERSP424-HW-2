1a81f4b07065e32957dd2b93131e80e4014524d5
gcdavis26 <geoffreycdavis@proton.me>
2024-03-24 20:45:25 -0400

    added sensor classes

--- Sensor.cpp
+++ Sensor.cpp
@@ -1,1 +1,62 @@
 #include "Sensor.h"
+#include <iostream>
+
+Altimeter::Altimeter()
+{
+	std::cout << "Altimter on" << std::endl;
+}
+Altimeter::~Altimeter()
+{
+	std::cout << "Altimeter off";
+}
+
+void Altimeter::processData()
+{
+	std::cout << "Processing data from the Altimeter Sensor" << std::endl;
+}
+
+void Altimeter::gatherData()
+{
+	std::cout << "Gathering data from the Altimeter Sensor" << std::endl;
+
+}
+
+Thermometer::Thermometer()
+{
+	std::cout << "Thermometer on" << std::endl;
+}
+Thermometer::~Thermometer()
+{
+	std::cout << "Thermometer off";
+}
+
+void Thermometer::processData()
+{
+	std::cout << "Processing data from the Thermometer Sensor" << std::endl;
+}
+
+void Thermometer::gatherData()
+{
+	std::cout << "Gathering data from the Thermometer Sensor" << std::endl;
+}
+
+
+FuelLevel::FuelLevel()
+{
+	std::cout << "FuelLevel on" << std::endl;
+}
+FuelLevel::~FuelLevel()
+{
+	std::cout << "FuelLevel off";
+}
+
+void FuelLevel::processData()
+{
+	std::cout << "Processing data from the FuelLevel Sensor" << std::endl;
+}
+
+void FuelLevel::gatherData()
+{
+	std::cout << "Gathering data from the FuelLevel Sensor" << std::endl;
+
+}
--- Sensor.h
+++ Sensor.h
@@ -1,1 +1,43 @@
-#
+#pragma once
+
+#ifndef SENSOR_H
+#define SENSOR_H
+
+class Sensor
+{
+protected:
+	virtual void gatherData() = 0;
+	virtual void processData() = 0;
+};
+
+class Altimeter : public Sensor
+{
+public:
+	Altimeter();
+	~Altimeter();
+	void gatherData();
+	void processData();
+
+};
+
+class Thermometer : public Sensor
+{
+public: 
+	Thermometer();
+	~Thermometer();
+	void gatherData();
+	void processData();
+
+};
+
+class FuelLevel : public Sensor
+{
+public:
+	FuelLevel();
+	~FuelLevel();
+	void gatherData();
+	void processData();
+
+};
+
+#endif SENSOR_H
