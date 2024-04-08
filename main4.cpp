1ab422a2cdb7fbf6316d030047b78649780d233d
gcdavis26 <geoffreycdavis@proton.me>
2024-04-08 14:10:17 -0400

    Added code for spiral

--- main4.cpp
+++ main4.cpp
@@ -1,8 +1,9 @@
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <cmath>
 #include <iostream>
+
+const double pi = 3.1415;
 
 int main() {
     if (!glfwInit()) {
@@ -10,7 +11,7 @@ int main() {
         return -1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(500, 500, "Sine Curve using OpenGL with GLEW and GLFW", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(500, 500, "Technicolor Spiral", NULL, NULL);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -29,10 +30,31 @@ int main() {
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_LINES);
+
+        float starting_x =0;
+        float starting_y= 0;
+
+        for (int deg = 0; deg < 1440; deg++)
+        {
+            glColor3f(cos(static_cast<float>(deg) * pi / 180), sin(static_cast<float>(deg) * pi / 180), .5); //color modulation
+            float x = cos(static_cast<float>(deg)* pi/180)*deg/1440;
+            float y = sin(static_cast<float>(deg) * pi / 180)*deg/1440;
+            //std::cout << "x: " << x << "  y: " << y << "   " << deg << std::endl;
+            glVertex2f(starting_x, starting_y);
+            glVertex2f(x, y);
+            starting_x = x;
+            starting_y = y;     
+        }
+          
+        glEnd();
+
+
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
-    }
+    return 0;
+}
